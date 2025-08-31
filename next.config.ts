62ed7a1d2b4cc0d19850910d79ef84524c7952b5
fosth112 <fosthkycreate@gmail.com>
2025-09-01 00:30:12 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,   // ข้าม ESLint error ตอน build
+  },
+  typescript: {
+    ignoreBuildErrors: true,    // ข้าม TS error ตอน build
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
